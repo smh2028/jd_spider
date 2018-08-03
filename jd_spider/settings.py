59ccc3d708ae52cc03c79a0126091db9bdb524b7
smh2028 <smh2208@163.com>
2018-08-03 21:57:24 +0800

    添加水大红贴，以及避免连续回复的判断。

--- jd_spider/settings.py
+++ jd_spider/settings.py
@@ -24,11 +24,8 @@ MONGO_DB = 'jd_goods'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-
-
-
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
